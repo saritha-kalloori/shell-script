15c8af871a432f699ff4b1b8a74a601200899496
saritha-91 <saritha.kalluri91@gmail.com>
2024-04-18 14:46:27 +0100

    variable-script

---
+++ 03-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "charlie:: hello saritha, how are you?"
+echo "saritha:: hello charlie,iam gud.how are you?"
+echo "charlie:: me too fine, how is your work?"
+echo "saritha::not bad, iam planning to learn devops"
