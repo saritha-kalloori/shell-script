4b4523344085332e2b59c517f9357ecf7d9c9423
saritha-91 <saritha.kalluri91@gmail.com>
2024-04-18 15:52:58 +0100

    another variable script

---
+++ 04-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=charlie
+PERSON2=saritha
+
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1,iam gud.how are you?"
+echo "$PERSON1:: me too fine, how is your work?"
+echo "$PERSON2::not bad, iam planning to learn devops"
