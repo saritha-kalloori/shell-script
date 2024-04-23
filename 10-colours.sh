60dd0a5d0b3d24c024b93df04622890e8ac1a411
saritha-91 <saritha.kalluri91@gmail.com>
2024-04-23 23:22:03 +0100

    10-colours.sh

--- 10-colours.sh
+++ 10-colours.sh
@@ -5,14 +5,19 @@ TIMESTAMP=$(date +%F-%H-%M-%s)
 SCRIPT_NAME=$($0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[33m"
+echo "script started executing at :$TIMESTAMP"
+
 VALIDATE(){
     if [ $1 -ne 0] 
     then
 
-    echo "$2...failure"
+    echo -e "$2...$R failure $N"
     exit 1
     else 
-    echo "$2...success"
+    echo  -e "$2...$G success"$N"
     fi
 }
 
