0a79ad7058fd5e92146c0aca5d5a1d1c1b1c72d8
saritha-91 <saritha.kalluri91@gmail.com>
2024-04-23 23:43:24 +0100

    colours-shell-script

--- 10-colours.sh
+++ 10-colours.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%s)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 R="\e[31m"
 G="\e[32m"
-N="\e[33m"
+N="\e[0m"
+
 echo "script started executing at :$TIMESTAMP"
 
 VALIDATE(){
     if [ $1 -ne 0] 
     then
 
-    echo -e "$2...$R failure $N"
-    exit 1
+       echo -e "$2...$R FAILURE $N"
+       exit 1
     else 
-    echo  -e "$2...$G success"$N"
+       echo  -e "$2...$G SUCCESS $N"
+
     fi
 }
 
@@ -26,8 +28,8 @@ VALIDATE(){
 if [ $USERID -ne 0 ]
 then
 
-  echo "please run this script with root access"
-  exit 1
+    echo  "please run this script with root access."
+    exit 1
 else
    echo "you are the super user"
 fi
