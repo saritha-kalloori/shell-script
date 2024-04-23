4100af70bbb19774767a9edcabdf6eac28622166
saritha-91 <saritha.kalluri91@gmail.com>
2024-04-24 00:58:32 +0100

    install- packages-shellscript

---
+++ 12-install-packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID= $(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0] 
+    then
+
+    echo "$2...failure"
+    exit 1
+    else 
+    echo "$2...success"
+    fi
+}
+
+if [$USERID -ne 0]
+ then 
+echo "please run this script with root access."
+exit 1
+
+  else 
+  echo " you are the super user"
+  fi
+  
+  for i in $@
+  do 
+  echo "package to install: $i"
+  dnf list installed $i &>>$LOGFILE
+  if [ $? -eq 0]
+  then
+      echo -e "$i already installed ...$Y SKIPPING $N"
+      else
+      echo "$i not installed.. need to install"
+      fi
